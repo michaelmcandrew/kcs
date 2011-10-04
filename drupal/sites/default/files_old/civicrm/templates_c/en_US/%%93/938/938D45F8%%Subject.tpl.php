b2bc6f53791846eef09ed9b357990f11cfdb4168
KatyMoon <katyjockelson@thirdsectordesign.org>
2011-10-04 11:00:45 +0100

    styling new full member list

--- drupal/sites/default/files_old/civicrm/templates_c/en_US/%%93/938/938D45F8%%Subject.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2011-06-06 16:12:05
-         compiled from CRM/Block/Subject.tpl */ ?>
-<span class='crm_blocktitle'><?php echo $this->_tpl_vars['subject']; ?>
-</span>
--- drupal/sites/default/files_old/civicrm/templates_c/en_US/%%F0/F0F/F0F71C33%%version.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2011-06-06 16:12:05
-         compiled from CRM/common/version.tpl */ ?>
-4.0.2
